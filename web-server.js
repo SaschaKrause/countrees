641e15319c698294d97675c5a89bddeccf354f6c
Sascha Krause <mr.sascha.krause@gmail.com>
2013-08-05 01:07:58 +0200

    added less support

--- web-server.js
+++ web-server.js
@@ -20,11 +20,19 @@ app.set('view engine', 'jade');
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+
+app.use(require('less-middleware')({
+  dest: path.join(__dirname, 'public', 'styles', 'dest'),
+  src: path.join(__dirname, 'public', 'styles', 'src'),
+  prefix: '/styles/dest',
+  force: true, // Always re-compile less files on each request.
+  compress: true
+})); // needs to be declared before static middleware (in order to get the recompile working)
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
 // serve index and view partials
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('index');
 });
 
